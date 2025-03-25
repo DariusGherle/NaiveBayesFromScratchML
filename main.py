63dc9eaf3a12d24cd4aaeebcd3570cc2ddc634d8
Darius Gherle <dariusgherle@gmail.com>
2025-03-25 03:22:56 +0200

    Added Model Evaluation and plottet ROC curve

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+"""
+Load raw emails
+    ↓
+Clean & lemmatize
+    ↓
+Transform to bag-of-words (CountVectorizer)
+    ↓
+Split train/test
+    ↓
+Train Naive Bayes:
+    - get_prior
+    - get_likelihood
+    ↓
+Predict on test (get_posterior)
+    ↓
+Evaluate accuracy
+    ↓
+Predict new emails manually
+"""
+
 import glob
 import numpy as np
 from nltk.corpus import names
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 
 
 # ==================== FUNCȚII UTILE ==================== #
@@ -131,3 +155,47 @@ print(f"The accuracy on {len(Y_test)} testing samples is: {accuracy:.1f}%")
 
 sample = "Congratulations! You won a free ticket to Bahamas. Claim now!"
 print("Predicție pentru emailul tău:", predict(sample))
+
+#====================== Model Score =================== #
+
+# All three separately
+#print(confusion_matrix(Y_test, predicted_labels, labels=[0,1]))
+#print(precision_score(Y_test, predicted_labels, pos_label=1))
+#print(recall_score(Y_test, predicted_labels, pos_label=1))
+#print(f1_score(Y_test, predicted_labels, pos_label=1))
+
+report = classification_report(Y_test, predicted_labels)
+print(report)
+
+#   Area Under Curve / Receiver Operating Characteristic
+# Extragem probabilitatea de SPAM pentru fiecare email
+pos_prob = np.array([p[1] for p in posterior])
+
+thresholds = np.arange(0, 1.2, 0.1)
+true_pos, false_pos = [0]*len(thresholds), [0]*len(thresholds)
+
+for pred, y in zip(pos_prob, Y_test):
+    for i, threshold in enumerate(thresholds):
+        if pred > threshold:
+            if y == 1:
+                true_pos[i] += 1
+            else:
+                false_pos[i] += 1
+        else:
+            break
+
+true_pos_rate =  [tp / sum(np.array(Y_test) == 1) for tp in true_pos]
+false_pos_rate = [fp / sum(np.array(Y_test) == 0) for fp in false_pos]
+
+# Plot the ROC curve
+plt.figure()
+lw = 2
+plt.plot(false_pos_rate, true_pos_rate, color='darkorange', lw=lw, label='ROC curve')
+plt.plot([0, 1],  [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
